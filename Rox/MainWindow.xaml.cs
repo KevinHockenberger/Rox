504ff37471afa3a2404e33805af3071c6b07fb85
khack247 <khack247@users.noreply.github.com>
2019-09-19 08:30:58 -0400

    basic tree gui

--- Rox/MainWindow.xaml.cs
+++ Rox/MainWindow.xaml.cs
@@ -1,77 +1,230 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace Rox
 {
-        public enum NodeTypes
+    public class DelegateCommand<T> : System.Windows.Input.ICommand where T : class
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+        public DelegateCommand(Action<T> execute)            : this(execute, null)
         {
-            General = 0,
-            Mode = 1,
-            Condition = 2,
-            Timer = 3
         }
-        public class IfeRoutedEventArgs : RoutedEventArgs
+        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
         {
-            public NodeTypes nodeType;
-            public string NodeInfo;
+            _execute = execute;
+            _canExecute = canExecute;
         }
-        public class IfeTreeNode : TreeViewItem
+        public bool CanExecute(object parameter)
         {
-            public event EventHandler<IfeRoutedEventArgs> NodeSelected;
-            public virtual void OnSelected(IfeRoutedEventArgs e) { NodeSelected?.Invoke(this, e); }
-            internal NodeTypes NodeType { get; set; }
-            internal readonly string HelperText;
-            public string NodeText { get; set; }
-            public int uId { get; set; }
-            //static IfeTreeNode()
-            //{
-            //    DefaultStyleKeyProperty.OverrideMetadata(typeof(IfeTreeNode), new FrameworkPropertyMetadata(typeof(IfeTreeNode)));
-            //}
-            public IfeTreeNode() { }
-            public IfeTreeNode(string name, NodeTypes nodeType)
-            {
+            if (_canExecute == null) { return true; }
+            return _canExecute((T)parameter);
+        }
 
-                NodeText = name;
-                NodeType = nodeType;
-                switch (nodeType)
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+        public event EventHandler CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+    public interface INode
+    {
+        NodeTypes NodeType { get; }
+        string Name { get; set; }
+        string Description();
+        List<INode> Items { get; set; }
+        List<NodeTypes> AllowedNodes { get; }
+    }
+    public enum NodeTypes
+    {
+        General = 0,
+        Mode = 1,
+        Condition = 2,
+        Timer = 3,
+        Initialized = 4,
+        Continuous = 5
+    }
+    public class IteMode : INode
+    {
+        public NodeTypes NodeType { get; } = NodeTypes.Mode;
+        public string Name { get; set; }
+        public List<NodeTypes> AllowedNodes { get; } = new List<NodeTypes>() { NodeTypes.Condition };
+        public List<INode> Items { get; set; } = new List<INode>();
+        public string Description() { return "{ MODE } A mode can be created to easily abort a running mode and start new sequencing. Stop and Auto modes will run with Start/Stop button. No items can be added directly. Add items to Initialize or Continuous branches."; }
+        public IteMode(string name)
+        {
+            Name = name;
+        }
+    }
+    public class IteFirstScan : INode
+    {
+        public NodeTypes NodeType { get; } = NodeTypes.Initialized;
+        public string Name { get; set; }
+        public List<NodeTypes> AllowedNodes { get; } = new List<NodeTypes>() { NodeTypes.Condition };
+        public List<INode> Items { get; set; } = new List<INode>();
+        public string Description() { return "{ First Scan branch } This sequence will run one time when the program is loaded and can accept any item."; }
+        public IteFirstScan(string name)
+        {
+            Name = name;
+        }
+    }
+    public class IteIntialize : INode
+    {
+        public NodeTypes NodeType { get; } = NodeTypes.Initialized;
+        public string Name { get; set; }
+        public List<NodeTypes> AllowedNodes { get; } = new List<NodeTypes>() { NodeTypes.Condition };
+        public List<INode> Items { get; set; } = new List<INode>();
+        public string Description() { return "{ Initialize branch } This sequence will only run one time when the mode is first started and before the continuous branch. This node can accept any item."; }
+        public IteIntialize(string name)
+        {
+            Name = name;
+        }
+    }
+    public class IteContinuous : INode
+    {
+        public NodeTypes NodeType { get; } = NodeTypes.Continuous;
+        public string Name { get; set; }
+        public List<NodeTypes> AllowedNodes { get; } = new List<NodeTypes>() { NodeTypes.Condition };
+        public List<INode> Items { get; set; } = new List<INode>();
+        public string Description() { return "{ Continuous branch } This sequence will run repeatedly after the initalize branch has completed and while the mode is active."; }
+        public IteContinuous(string name)
+        {
+            Name = name;
+        }
+    }
+    public class IteNodeViewModel : INotifyPropertyChanged
+    {
+        public DelegateCommand<string> ButtonClickCommand
+        {
+            get { return _clickCommand; }
+        }
+        private readonly DelegateCommand<string> _clickCommand;
+        private INode Node { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool _isSelected;
+        private bool _isExpanded = true;
+        public IteNodeViewModel Parent { get; set; }
+        public Collection<IteNodeViewModel> Items { get; set; }
+        public IteNodeViewModel(INode node) : this(node, null) { }
+        public IteNodeViewModel(INode node, IteNodeViewModel parent)
+        {
+            Node = node;
+            Parent = parent;
+            Items = new Collection<IteNodeViewModel>();
+            foreach (var item in node.Items)
+            {
+                switch (item.NodeType)
                 {
+                    case NodeTypes.General:
+                        Items.Add(new IteNodeViewModel(item));
+                        break;
                     case NodeTypes.Mode:
-                        HelperText = name + " Mode.\r" +
-                            "  \u2022 Items can only be added to [Initialize] and\\or [Continuous]. \r" +
-                            "  \u2022 The Initialize sequence will only run one time when the mode is first started. \r" +
-                            "  \u2022 The Continuous sequence will run repeatedly until the mode is no longer active.";
+                        Items.Add(new IteMODE_VM(item));
                         break;
                     case NodeTypes.Condition:
-                        HelperText = "If-Then-Else conditional statement.";
+                        Items.Add(new IteNodeViewModel(item));
                         break;
                     case NodeTypes.Timer:
-                        HelperText = "Timer.";
+                        Items.Add(new IteTIMER_VM(item));
+                        break;
+                    case NodeTypes.Initialized:
+                        Items.Add(new IteFIRST_VM(item));
+                        break;
+                    case NodeTypes.Continuous:
+                        Items.Add(new IteCONTINUOUS_VM(item));
                         break;
                     default:
-                        HelperText = name + " general node.\r" +
-                            "  \u2022 Most items are allowed here.";
                         break;
                 }
-                Selected += (sender, eventargs) =>
+            }
+            //Items = new Collection<IteNodeViewModel>((from item in node.Items select new IteNodeViewModel(item, this)).ToList<IteNodeViewModel>());
+            _clickCommand = new DelegateCommand<string>(
+                       (s) => 
+                       {
+                           Console.WriteLine(node.Description());
+                       }, //Execute
+                       (s) => { return true; } //CanExecute
+                       );
+        }
+        public string Name
+        {
+            get { return Node.Name; }
+        }
+        public string Description
+        {
+            get { return Node.Description(); }
+        }
+        public NodeTypes NodeType
+        {
+            get { return Node.NodeType; }
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (value != _isSelected)
                 {
-                    OnSelected(new IfeRoutedEventArgs() { nodeType = nodeType, NodeInfo = HelperText });
-                    eventargs.Handled = true;
-                };
-                //Style = (Style)FindResource("TreeViewItem");
-                //SetResourceReference(StyleProperty, typeof(IfeTreeNode));
+                    _isSelected = value;
+                    this.OnPropertyChanged("IsSelected");
+                }
             }
         }
+        public bool IsExpanded
+        {
+            get { return _isExpanded; }
+            set
+            {
+                if (value != _isExpanded)
+                {
+                    _isExpanded = value;
+                    this.OnPropertyChanged("IsExpanded");
+                }
+
+                // Expand all the way up to the root.
+                if (_isExpanded && Parent != null)
+                    Parent.IsExpanded = true;
+            }
+        }
+    }
+    public class IteCONTINUOUS_VM : IteNodeViewModel
+    {
+        public IteCONTINUOUS_VM(INode node) : base(node) { }
+    }
+    public class IteMODE_VM : IteNodeViewModel
+    {
+        public IteMODE_VM(INode node) : base(node) { }
+    }
+    public class IteFIRST_VM : IteNodeViewModel
+    {
+        public IteFIRST_VM(INode node) : base(node) { }
+    }
+    public class IteTIMER_VM : IteNodeViewModel
+    {
+        public IteTIMER_VM(INode node) : base(node) { }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        public List<IteNodeViewModel> Modes;
         System.Threading.Timer closeMnu;
         private bool? _running;
         public bool? Running
@@ -275,74 +428,16 @@ namespace Rox
         }
         private void SetDefaultGuiElements()
         {
-            tree.Items.Clear();
-            tree.Items.Add(GetGeneralNode("1st scan"));
-            tree.Items.Add(GetBasicNode("Stop"));
-            tree.Items.Add(GetBasicNode("Auto"));
-            tree.Items.Add(GetModeNode("Stop"));
-            tree.Items.Add(GetModeNode("Auto"));
-        }
-        private IfeTreeNode GetGeneralNode(string nodeName)
-        {
-            var ret = new IfeTreeNode(nodeName, NodeTypes.General)
+            Modes = new List<IteNodeViewModel>()
             {
-                Header = new StackPanel()
-                {
-                    Orientation = Orientation.Horizontal,
-                    Children = {
-                        new Image() {Width=20,Height=20,Source= new BitmapImage(new Uri(@"pack://application:,,,/include/once.png", UriKind.Absolute)) },
-                        new Label() { Foreground = Brushes.White, Content = nodeName }
-                    }
-                }
+                new IteFIRST_VM( new IteFirstScan("1st scan")),
+                new IteMODE_VM( new IteMode("Stop"){Items={new IteIntialize("Initialize"),new IteContinuous("Continuous") } }),
+                new IteMODE_VM( new IteMode("Auto"){Items={new IteIntialize("Initialize"),new IteContinuous("Continuous") } }),
             };
-            ret.NodeSelected += new EventHandler<IfeRoutedEventArgs>(NodeSelectedEvent);
-            return ret;
-        }
-        private IfeTreeNode GetModeNode(string modeName)
-        {
-            var ret = new IfeTreeNode(modeName, NodeTypes.Mode)
+            tree.DataContext = new
             {
-                Foreground = Brushes.White,
-                IsExpanded = true,
-                Header = new StackPanel()
-                    {
-                        Orientation = Orientation.Horizontal,
-                        Children = {
-                        new Image() {Width=20,Height=20,Source= new BitmapImage(new Uri(@"pack://application:,,,/include/mode.png", UriKind.Absolute)) },
-                        new Label() { Foreground = Brushes.White, Content = modeName }
-                    }
-                },
-                Items = {
-                    new IfeTreeNode("Initialize", NodeTypes.General) {
-                    Header = new StackPanel()
-                    {
-                    Orientation =Orientation.Horizontal,
-                    Children = {
-                        new Image() {Width=20,Height=20,Source= new BitmapImage(new Uri(@"pack://application:,,,/include/once.png", UriKind.Absolute)) },
-                        new Label() { Foreground = Brushes.White, Content = "Initialize" }
-                    }
-                  } },
-                    new IfeTreeNode("Continuous", NodeTypes.General) {
-                    Header = new StackPanel()
-                    {
-                    Orientation =Orientation.Horizontal,
-                    Children = {
-                        new Image() {Width=20,Height=20,Source= new BitmapImage(new Uri(@"pack://application:,,,/include/continue.png", UriKind.Absolute)) },
-                        new Label() { Foreground = Brushes.White, Content = "Continuous" }
-                    }
-                  } } }
+                Modes
             };
-            ret.NodeSelected += new EventHandler<IfeRoutedEventArgs>(NodeSelectedEvent);
-            foreach (var item in ret.Items)
-            {
-                ((IfeTreeNode)item).NodeSelected += new EventHandler<IfeRoutedEventArgs>(NodeSelectedEvent);
-            }
-            return ret;
-        }
-        private TreeViewItem GetBasicNode(string modeName)
-        {
-            var ret = new TreeViewItem() { Header = "abc", Items = { new TreeViewItem() { Header = "123" } } };
-            return ret;
         }
         private void btnLoadFile_Click(object sender, RoutedEventArgs e)
         {
@@ -395,13 +490,6 @@ namespace Rox
             //    listRecentFiles.SelectedItem = listRecentFiles.Items[0];
             //}
         }
-        private void NodeSelectedEvent(object sender, IfeRoutedEventArgs eventArgs)
-        {
-            //Dispatcher.Invoke(() => {
-            SetHelperText(eventArgs.NodeInfo);
-            SetAvailableAddButtons(eventArgs.nodeType);
-            //});
-        }
         private void SetHelperText(string t)
         {
             txtSelectedNodeInfo.Text = t;
@@ -423,17 +511,23 @@ namespace Rox
             }
         }
 
-        private void AddMode_Click(object sender, RoutedEventArgs e)
+        private void tvi_GotFocus(object sender, RoutedEventArgs e)
         {
-            NodeParamsWindow f = new NodeParamsWindow(null);
-            if (f.ShowDialog() == true)
-            {
-                Console.WriteLine(f.NodeName);
-                if (!string.IsNullOrWhiteSpace(f.NodeName))
-                {
-                    tree.Items.Add(GetModeNode(f.NodeName));
-                }
-            }
+            //Dispatcher.Invoke(() => {
+            var s = ((IteNodeViewModel)((TreeViewItem)sender).DataContext);
+            SetHelperText(s.Name + " - " + s.Description);
+            SetAvailableAddButtons(((IteNodeViewModel)((TreeViewItem)sender).DataContext).NodeType);
+            //});
+            e.Handled = true;
         }
+
+        //private void tvi_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    e.Handled = true;
+        //}
+        //private void tvi_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    e.Handled = true;
+        //}
     }
 }
